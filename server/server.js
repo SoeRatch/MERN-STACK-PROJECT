cb9118a012b780f133d938b7f6d14fefa1ba258c
Suraj Sharma <surajkoho@gmail.com>
2017-12-01 22:12:40 +0530

    bodyparser and cors added

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
 import http from 'http';
 import express from 'express';
-
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 
 
 const app = express();
 app.server = http.createServer(app);
 
+//CORS - 3RD party middleware
+app.use(cors());
+
+app.use(bodyParser.json({extended: false}));
 
 app.get('/',(req,res)=> res.send("Initial Application"));
 
